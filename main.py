736bb1539234479a6b0e74d7840b521e4da46859
RAGE-12 <manishshankar43@gmail.com>
2023-05-21 00:15:14 +0530

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import numpy as np
 
-# Define global variables
 recording = False
 output_file_prefix = 'recording'
 output_file_extension = '.avi'
@@ -14,27 +13,23 @@ recording_number = 1
 def start_recording():
     global recording, recording_number
     recording = True
-    screen_size = pyautogui.size()  # Get the screen size dynamically
+    screen_size = pyautogui.size()
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     output_file = f'{output_file_prefix}{recording_number}{output_file_extension}'
     recording_number += 1
     out = cv2.VideoWriter(output_file, fourcc, 20.0, screen_size)
 
-    logo = cv2.imread('C:/Users/KIIT/OneDrive/Documents/pythonProject/screen recorder/logo.png', cv2.IMREAD_UNCHANGED)  # Load the logo image with alpha channel
-    # specify desired width
+    logo = cv2.imread('logo.png', cv2.IMREAD_UNCHANGED)
     logo_width = 100
     logo_height = 100
-    logo_resized = cv2.resize(logo, (logo_width, logo_height))  # Resize the logo to a desired size
+    logo_resized = cv2.resize(logo, (logo_width, logo_height))
 
     while recording:
         img = pyautogui.screenshot()
         frame = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-
-        # Define the region where the logo will be placed (e.g., top-right corner)
         x_offset = frame.shape[1] - logo_resized.shape[1] - 10
         y_offset = 10
 
-        # Blend the logo with the frame using alpha channel
         alpha_s = logo_resized[:, :, 3] / 255.0
         alpha_l = 1.0 - alpha_s
         for c in range(0, 3):
@@ -58,16 +53,13 @@ def start_recording_thread():
     recording_thread.start()
 
 
-# Create a simple UI using Tkinter
 root = tk.Tk()
 root.title("Screen Recorder")
 
-# Load the logo image using PIL
-logo_image = Image.open('C:/Users/KIIT/OneDrive/Documents/pythonProject/screen recorder/logo.png')
+logo_image = Image.open('logo.png')
 logo_image = logo_image.resize((300, 300), Image.ANTIALIAS)
 logo_tk = ImageTk.PhotoImage(logo_image)
 
-# Create a label to display the logo
 logo_label = tk.Label(root, image=logo_tk)
 logo_label.pack(pady=10)
 
